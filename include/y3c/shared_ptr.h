70265799e0c6f461e218e988932bb50bb3d7c00d
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-11-26 02:40:37 +0900

    castを諦めればシンプルになる?

--- include/y3c/shared_ptr.h
+++ include/y3c/shared_ptr.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "y3c/terminate.h"
 #include <memory>
+#include <type_traits>
 
 namespace y3c {
 
+/*!
+ * y3c::shared_ptr は `*ptr` と `ptr->` 使用時にnullptrチェックを行う。
+ *
+ * y3c::strict::shared_ptr はそれに加えて `get()`, `y3c::unwrap()`
+ * でもnullptrチェックを行う。
+ *
+ */
 template <typename T, bool StrictMode = false>
 class shared_ptr {
     std::shared_ptr<T> base_;
@@ -29,33 +37,37 @@ class shared_ptr {
     shared_ptr &operator=(shared_ptr &&) = default;
     ~shared_ptr() = default;
 
-    template <typename... Args>
-    shared_ptr(Args &&...args) : base_(std::forward<Args>(args)...) {}
-    template <typename Arg>
-    shared_ptr &operator=(Arg &&arg) {
-        base_ = arg;
+    shared_ptr(const std::shared_ptr<T> &ptr) noexcept : base_(ptr) {}
+    shared_ptr(std::shared_ptr<T> &&ptr) noexcept : base_(std::move(ptr)) {}
+    shared_ptr &operator=(const std::shared_ptr<T> &ptr) noexcept {
+        base_ = ptr;
+        return *this;
+    }
+    shared_ptr &operator=(std::shared_ptr<T> &&ptr) noexcept {
+        base_ = std::move(ptr);
         return *this;
     }
 
-    operator std::shared_ptr<T>() const { return check_(); }
+    std::shared_ptr<T> unwrap() const { return check_(); }
 
-    template <typename... Args>
-    void reset(Args &&...args) {
-        base_.reset(std::forward<Args>(args)...);
-    }
-
-    void swap(std::shared_ptr<T> &other) noexcept { base_.swap(other); }
+    void reset() noexcept { base_.reset(); }
     void swap(shared_ptr &other) noexcept { this->swap(other.base_); }
 
-    template <typename U = T>
+    template <typename U = T,
+              typename std::enable_if<std::is_same<U, T>::value,
+                                      std::nullptr_t>::type = nullptr>
     U *get() const {
         return check_().get();
     }
-    template <typename U = T>
+    template <typename U = T,
+              typename std::enable_if<std::is_same<U, T>::value,
+                                      std::nullptr_t>::type = nullptr>
     U &operator*() const {
         return *check_strict_();
     }
-    template <typename U = T>
+    template <typename U = T,
+              typename std::enable_if<std::is_same<U, T>::value,
+                                      std::nullptr_t>::type = nullptr>
     U *operator->() const {
         return check_strict_().get();
     }
@@ -67,11 +79,76 @@ class shared_ptr {
 
     long use_count() const noexcept { return base_.use_count(); }
     explicit operator bool() const noexcept { return static_cast<bool>(base_); }
-    template <typename Arg>
-    bool owner_before(const Arg &arg) const {
-        return base_.owner_before(arg);
+    bool owner_before(const shared_ptr &arg) const {
+        return base_.owner_before(arg.base_);
+    }
+
+    friend bool operator==(const shared_ptr &lhs,
+                           const shared_ptr &rhs) noexcept {
+        return lhs.base_ == rhs.base_;
+    }
+    friend bool operator!=(const shared_ptr &lhs,
+                           const shared_ptr &rhs) noexcept {
+        return lhs.base_ != rhs.base_;
+    }
+    friend bool operator<(const shared_ptr &lhs,
+                          const shared_ptr &rhs) noexcept {
+        return lhs.base_ < rhs.base_;
+    }
+    friend bool operator<=(const shared_ptr &lhs,
+                           const shared_ptr &rhs) noexcept {
+        return lhs.base_ <= rhs.base_;
+    }
+    friend bool operator>(const shared_ptr &lhs,
+                          const shared_ptr &rhs) noexcept {
+        return lhs.base_ > rhs.base_;
+    }
+    friend bool operator>=(const shared_ptr &lhs,
+                           const shared_ptr &rhs) noexcept {
+        return lhs.base_ >= rhs.base_;
+    }
+    template <class CharT, class Traits>
+    friend std::basic_ostream<CharT, Traits> &
+    operator<<(std::basic_ostream<CharT, Traits> &os, const shared_ptr &p) {
+        return os << p.base_;
+    }
+
+    template <typename U>
+    shared_ptr<U> static_pointer_cast() const {
+        return std::static_pointer_cast<U>(base_);
+    }
+    template <typename U>
+    shared_ptr<U> dynamic_pointer_cast() const {
+        return std::dynamic_pointer_cast<U>(base_);
+    }
+    template <typename U>
+    shared_ptr<U> const_pointer_cast() const {
+        return std::const_pointer_cast<U>(base_);
     }
 };
+
+template <typename T, bool StrictMode>
+std::shared_ptr<T> unwrap(const shared_ptr<T, StrictMode> &ptr) {
+    return ptr.unwrap();
+}
+template <typename T, bool StrictMode>
+void swap(shared_ptr<T, StrictMode> &lhs,
+          shared_ptr<T, StrictMode> &rhs) noexcept {
+    lhs.swap(rhs);
+}
+template <typename U, typename T, bool StrictMode>
+shared_ptr<U> static_pointer_cast(const shared_ptr<T, StrictMode> &r) {
+    return r.template static_pointer_cast<U>();
+}
+template <typename U, typename T, bool StrictMode>
+shared_ptr<U> dynamic_pointer_cast(const shared_ptr<T, StrictMode> &r) {
+    return r.template dynamic_pointer_cast<U>();
+}
+template <typename U, typename T, bool StrictMode>
+shared_ptr<U> const_pointer_cast(const shared_ptr<T, StrictMode> &r) {
+    return r.template const_pointer_cast<U>();
+}
+
 
 namespace strict {
 template <typename T>
--- tests/shared_ptr.cc
+++ tests/shared_ptr.cc
@@ -17,7 +17,7 @@ TEST(y3c, shared_ptr) {
         EXPECT_EQ(a.get()->val, 42);
         EXPECT_EQ(a.use_count(), 1L);
         EXPECT_TRUE(static_cast<bool>(a));
-        EXPECT_TRUE(static_cast<bool>(static_cast<std::shared_ptr<A>>(a)));
+        EXPECT_TRUE(static_cast<bool>(y3c::unwrap(a)));
 
         a.reset();
         EXPECT_THROW((*a).val, y3c::internal::exception_terminate);
@@ -25,7 +25,7 @@ TEST(y3c, shared_ptr) {
         EXPECT_EQ(a.get(), nullptr);
         EXPECT_EQ(a.use_count(), 0L);
         EXPECT_FALSE(static_cast<bool>(a));
-        EXPECT_FALSE(static_cast<bool>(static_cast<std::shared_ptr<A>>(a)));
+        EXPECT_FALSE(static_cast<bool>(y3c::unwrap(a)));
     }
     {
         y3c::strict::shared_ptr<A> a2 = std::make_shared<A>(42);
@@ -34,7 +34,7 @@ TEST(y3c, shared_ptr) {
         EXPECT_EQ(a2.get()->val, 42);
         EXPECT_EQ(a2.use_count(), 1L);
         EXPECT_TRUE(static_cast<bool>(a2));
-        EXPECT_TRUE(static_cast<bool>(static_cast<std::shared_ptr<A>>(a2)));
+        EXPECT_TRUE(static_cast<bool>(y3c::unwrap(a2)));
 
         a2.reset();
         EXPECT_THROW((*a2).val, y3c::internal::exception_terminate);
@@ -42,7 +42,6 @@ TEST(y3c, shared_ptr) {
         EXPECT_THROW(a2.get(), y3c::internal::exception_terminate);
         EXPECT_EQ(a2.use_count(), 0L);
         EXPECT_FALSE(static_cast<bool>(a2));
-        EXPECT_THROW(static_cast<std::shared_ptr<A>>(a2),
-                     y3c::internal::exception_terminate);
+        EXPECT_THROW(y3c::unwrap(a2), y3c::internal::exception_terminate);
     }
 }
