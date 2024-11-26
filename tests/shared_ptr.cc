b521cd402c594689d77c46318b6727a32bc2cd95
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-11-26 12:07:22 +0900

    gtestをdoctestに変更

--- tests/shared_ptr.cc
+++ tests/shared_ptr.cc
@@ -1,47 +1,47 @@
 #include <y3c/shared_ptr.h>
 #include <y3c/exception.h>
-#include <gtest/gtest.h>
+#include <doctest.h>
 
 struct A {
     A(int val) : val(val) {}
     int val;
 };
 
-TEST(y3c, shared_ptr) {
+TEST_CASE("shared_ptr") {
     y3c::internal::enable_throw_terminate();
 
     {
         y3c::shared_ptr<A> a = std::make_shared<A>(42);
-        EXPECT_EQ((*a).val, 42);
-        EXPECT_EQ(a->val, 42);
-        EXPECT_EQ(a.get()->val, 42);
-        EXPECT_EQ(a.use_count(), 1L);
-        EXPECT_TRUE(static_cast<bool>(a));
-        EXPECT_TRUE(static_cast<bool>(y3c::unwrap(a)));
+        CHECK_EQ((*a).val, 42);
+        CHECK_EQ(a->val, 42);
+        CHECK_EQ(a.get()->val, 42);
+        CHECK_EQ(a.use_count(), 1L);
+        CHECK(static_cast<bool>(a));
+        CHECK(static_cast<bool>(y3c::unwrap(a)));
 
         a.reset();
-        EXPECT_THROW((*a).val, y3c::internal::exception_terminate);
-        EXPECT_THROW(a->val, y3c::internal::exception_terminate);
-        EXPECT_EQ(a.get(), nullptr);
-        EXPECT_EQ(a.use_count(), 0L);
-        EXPECT_FALSE(static_cast<bool>(a));
-        EXPECT_FALSE(static_cast<bool>(y3c::unwrap(a)));
+        CHECK_THROWS_AS((*a).val, y3c::internal::exception_terminate);
+        CHECK_THROWS_AS(a->val, y3c::internal::exception_terminate);
+        CHECK_EQ(a.get(), nullptr);
+        CHECK_EQ(a.use_count(), 0L);
+        CHECK_FALSE(static_cast<bool>(a));
+        CHECK_FALSE(static_cast<bool>(y3c::unwrap(a)));
     }
     {
         y3c::strict::shared_ptr<A> a2 = std::make_shared<A>(42);
-        EXPECT_EQ((*a2).val, 42);
-        EXPECT_EQ(a2->val, 42);
-        EXPECT_EQ(a2.get()->val, 42);
-        EXPECT_EQ(a2.use_count(), 1L);
-        EXPECT_TRUE(static_cast<bool>(a2));
-        EXPECT_TRUE(static_cast<bool>(y3c::unwrap(a2)));
+        CHECK_EQ((*a2).val, 42);
+        CHECK_EQ(a2->val, 42);
+        CHECK_EQ(a2.get()->val, 42);
+        CHECK_EQ(a2.use_count(), 1L);
+        CHECK(static_cast<bool>(a2));
+        CHECK(static_cast<bool>(y3c::unwrap(a2)));
 
         a2.reset();
-        EXPECT_THROW((*a2).val, y3c::internal::exception_terminate);
-        EXPECT_THROW(a2->val, y3c::internal::exception_terminate);
-        EXPECT_THROW(a2.get(), y3c::internal::exception_terminate);
-        EXPECT_EQ(a2.use_count(), 0L);
-        EXPECT_FALSE(static_cast<bool>(a2));
-        EXPECT_THROW(y3c::unwrap(a2), y3c::internal::exception_terminate);
+        CHECK_THROWS_AS((*a2).val, y3c::internal::exception_terminate);
+        CHECK_THROWS_AS(a2->val, y3c::internal::exception_terminate);
+        CHECK_THROWS_AS(a2.get(), y3c::internal::exception_terminate);
+        CHECK_EQ(a2.use_count(), 0L);
+        CHECK_FALSE(static_cast<bool>(a2));
+        CHECK_THROWS_AS(y3c::unwrap(a2), y3c::internal::exception_terminate);
     }
 }
