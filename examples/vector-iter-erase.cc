c3d458371b756aa46cf4231436bbfdf120903b09
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-12-28 20:15:49 +0900

    linuxでビルド通らなかった

--- examples/vector-iter-erase.cc
+++ examples/vector-iter-erase.cc
@@ -3,7 +3,7 @@
 int main() {
     y3c::vector<int> a = {1, 2, 3, 4, 5};
     for (y3c::vector<int>::iterator it = a.begin(); it < a.end(); ++it) {
-        if (*it == 3) {
+        if (unwrap(*it) == 3) {
             a.erase(it);
         }
     }
